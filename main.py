724768be25140bea59942105b1dc2b76b9eb8609
Abram Matthew <80390361+abram-matthew@users.noreply.github.com>
2022-02-04 18:58:00 +0500

    Fixed Order Issue
    Using Ctrl+Select to pick up files would indicate the order in which the documents would now be merged.

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from tkinter import filedialog
 # Prevents an empty tkinter window from appearing
 tkinter.Tk().withdraw() 
 #Getting the path of selected files in an array
-source_dir = filedialog.askopenfilenames() 
+source_dir = filedialog.askopenfilenames(filetypes=[('PDF files', '*.pdf'),
+                                                       ('JPG files', '*.jpg'),
+                                                       ('PNG files', '*.png'),
+                                                       ('all files', '.*')],
+                                            initialdir=os.getcwd(),
+                                            title="Select files", multiple=True) 
 #Making merger object
 merger =PdfFileMerger()
 
+source_dir = list(source_dir)
+#fixes tkinter issue: Bring first element to the last
+source_dir.append(source_dir.pop(0))  
 for item in source_dir:
     if item.endswith('pdf'):
         #appending all pdfs selections only
